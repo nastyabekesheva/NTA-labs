63a058478bd9b2ccbaad94a16442a4b1e2475e9a
nastyabekesheva <nastya.bekesheva@gmail.com>
2024-03-14 09:35:57 +0200

    add slvr

--- lab-1/Factorizer.py
+++ lab-1/Factorizer.py
@@ -5,11 +5,34 @@ import numpy as np
 def xor(a, b):
     return np.logical_xor(a, b).astype(int)
 
+def is_array_nonzero(arr):
+    return np.any(arr != 0)
+
+def replace_none_with_zero(arr, k):
+    return [x if x is not None else np.zeros(k) for x in arr]
+
+def any_non_zero(arr):
+    return any(any(x) for x in arr)
+
+def remove_none_rows(matrix):
+    return [row for row in matrix if row is not None]
+
+def check_matching_positions(arrays):
+    result = np.all(arrays, axis=0)
+    
+    return result.any()
+
+def find_index_of_nonzero_array(matrix):
+    return np.nonzero(np.any(matrix, axis=1))[0] # Return None if no row has non-zero elements
+
+def fx(x, n):
+    return (x**2+1) % n
+
 class Factorizer:
-    def __init__(self, n, f=None, method="rho-pollard", patience=50):
+    def __init__(self, n, f=fx, method="rho-pollard", patience=50):
         self.n = n
         self.f = f
-        self.mathod = method
+        self.method = method
         self.patience = patience
 
     def factorize(self):
@@ -35,6 +58,23 @@ class Factorizer:
                 d = self.__trial_division__(p)
                 if d != None:
                     return d, int(self.n/d)
+                
+            return None, None
+        
+        elif self.method == "brillhart-morrison":
+            a = 1 / math.sqrt(2)
+            res = None
+            i = 0
+
+            while res == None or self.patience >= i:
+                x, y = self.__brillhart_morrison__(a)
+                if x and y.is_integer():
+                    d = np.gcd((x+int(y)), self.n)
+
+                    if d > 1 and d < self.n:
+                        return d, int(self.n/d)
+                i += 1
+                a += 0.5
                 
             return None, None
 
@@ -64,7 +104,7 @@ class Factorizer:
         # sieve of eratosthenes
         prime = [True] * (self.n + 1)
         i = 2
-        while i**2 <= self.n:
+        while i**2 <= self.n and i <= 47:
             if prime[i] == True: # if prime[p] is not changed, then it is a prime         
                 for j in range(i**2, self.n + 1, i): # update all multiples of p
                     prime[j] = False
@@ -88,19 +128,45 @@ class Factorizer:
         if n == self.n:
             return m
         
-    def __brillhart_morrison__(self):
-        base = self.__generate_factor_base__()
-        cf = self.__generate_continued_fraction__(len(base))
+    def __brillhart_morrison__(self, a):
+        base = self.__generate_factor_base__(a)
+        cf = self.__generate_continued_fraction__(len(base)-1)
         sn = self.__generate_smooth_numbers__(cf)
-        s = [self.__find_prime_degree__(base, sn[i]**2) for i in range(len(sn))]
-        x, y = 1, 1
-        for i in range(len(base)):
-            x *= sn[i]**s[i]
-        for i in base:
-            y *= sn[i]**s[i]
+        s = [self.__find_mod_prime_degrees__(base, pow(sn[i], 2, self.n)) for i in range(len(sn))]
+        s = np.array(replace_none_with_zero(s, len(base)))
+        if s.any():
+            if len(find_index_of_nonzero_array(s)) > 1:
+                s2 = s % 2
+                xs = self.__solve_slr__(np.array(s2))
+                if len(xs) > 0:
+                    x, y = 1, 1
+                    for i in range(len(base)):
+                        x = (x * sn[i]**xs[i]) % self.n
+                    pows = []
+                    for i in range(len(base)-1):
+                        tmp = 0
+                        for j in range(len(s)):
+                            #f = sum([xs[0][j]*s[i][j] for j in range(len(base))]) 
+                            tmp += xs[j]*s[j][i]
+                        pows.append(tmp)
+                        #y = (y * base[i]**f) % self.n
+                    if pows[0] % 2 != 0:
+                        return None, None
+                    else:
+                        pows[0] = 0
+                        pows = [i/2 for i in pows]
+                    for i in range(len(base)-1):
+                        y = (y * base[i]**pows[i]) % self.n
+                    return x, y
+                else:
+                    return None, None
+            else:
+                return None, None
+        else:
+            return None, None
         
         
-    def __generate_factor_base__(self):
+    def __generate_factor_base__(self, a):
         prime = [True] * (self.n + 1)
         i = 2
         while i**2 <= self.n:
@@ -109,13 +175,13 @@ class Factorizer:
                     prime[j] = False
             i += 1
 
-        L = sympy.exp((sympy.log(self.n) / sympy.log(sympy.log(self.n)))**0.5)**(1 / math.sqrt(2))
-        primes = []
-        p = 2
+        L = sympy.exp((sympy.log(self.n) / sympy.log(sympy.log(self.n)))**0.5)**(a)
+        primes = [-1, 2]
+        p = 3  # Start from 3 to ensure an odd number
         while p < L**(1/np.sqrt(2)):
             if prime[p] and sympy.legendre_symbol(self.n, p) == 1:
                 primes.append(p)
-            p += 1
+            p += 2  # Increment by 2 to consider only odd numbers
         return primes
     
     def __generate_continued_fraction__(self, k):
@@ -127,54 +193,101 @@ class Factorizer:
         for i in range(k):
             v.append((self.n-u[i]**2) / v[i])
             alpha.append((math.sqrt(self.n)+u[i]) / v[i+1])
-            a.append(int(alpha[i]))
+            a.append(int(alpha[i+1]))
             u.append(a[i+1]*v[i+1]-u[i])
         
-        return a
+        return a[0:]
     
     def __generate_smooth_numbers__(self, a):
-        b = [1, 0]
+        b = []
         for i in range(len(a)):
-            b.append(a[i]*b[i-1]+b[i-2])
+            if i == 0:
+                b.append((a[i]*1) % self.n)
+            elif i == 1:
+                b.append((a[i]*b[0]+1) % self.n)
+            else:
+                b.append((a[i]*b[i-1]+b[i-2]) % self.n)
 
         return b
     
-    def __solve_slr__(self, s):
-        m, n = s.shape
-        full_solution = []
+    def __matrix_reduction__(self, matrix):
+        det = []
+        nz = find_index_of_nonzero_array(matrix)
+        m, n = matrix.shape
+        for j in range(n):
+            if len(det) == len(nz)-1:
+                break
+            i = np.where(matrix[:, j] == 1)[0]
+            if i.size != 0: 
+                i = i[0]
+                det.append(i)
+                rang = list(range(n))
+                rang.remove(j)
+                for k in rang:
+                    if matrix[i, k] == 1:
+                        matrix[:, k] = xor(matrix[:, j], matrix[:, k])
+        ud = set(range(m)).difference(set(det))
+        undet = []
+        for i in ud:
+            undet.append(i)
 
-        undetermined_rows = np.ones((m, 1), dtype=bool)
-        while np.any(undetermined_rows):
-            vector = np.zeros((n, 1), dtype=int)
-            solution = []
+        return (matrix, sorted(det), sorted(undet))
 
-            for i in range(n):
-                for x in range(m):
-                    if undetermined_rows[x] and vector[i] == s[x, i] == 1:
-                        vector = xor(vector, s[x])
-                        solution.append(s[x])
+    def __solve_slr__(self, matrix):
+        matrix, det, undet = self.__matrix_reduction__(matrix)
+        m, n = matrix.shape
+        '''full_solution = []
+        for x in undet:
+            solution = np.zeros(n)
+            for y in det:
+                if np.where((matrix[x] == 1) & (matrix[y] == 1))[0].size != 0:
+                    matrix[x] = xor(matrix[x], matrix[y])
+                    solution = xor(solution, matrix[x])
+            if (np.nonzero(solution)[0].size != 0):
+                full_solution.append(solution)'''
+        undet_nz = find_index_of_nonzero_array(matrix[undet])[0]
+        solution = []
+        for i in range(m):
+            if i == undet[undet_nz]:
+                solution.append(1)
+            else:
+                if check_matching_positions([matrix[i], matrix[undet[undet_nz]]]):
+                    solution.append(1)
+                else:
+                    solution.append(0)
 
-            if np.all(vector == 0):
-                full_solution.append(solution)
+        for i in range(n-len(solution)):
+            solution.append(0)
 
-            undetermined_rows = np.any(s[undetermined_rows] == 1, axis=1)
-
-        return full_solution
+        return solution
     
-    def __find_prime_degree__(self, factor_base, target_number):
-        # Construct the matrix A
-        A = np.array(factor_base).reshape(-1, 1)
+    def __find_mod_prime_degrees__(self, base, num):
+        x1 =  self.__find_prime_degrees__(base, num)
+        x2 =  self.__find_prime_degrees__(base, num-self.n)
 
-        # Construct the vector b
-        b = np.array([target_number])
+        if is_array_nonzero(np.array(x1)) and x1 != None:
+            return np.array(x1)
+        elif is_array_nonzero(np.array(x2)) and x2 != None:
+            return np.array(x2)
+        else:
+            return None
+    
+    def __find_prime_degrees__(self, base, num):
 
-        # Solve the linear equation Ax = b
-        x, _, _, _ = np.linalg.lstsq(A, b, rcond=None)
+        d = [0] * len(base)
+        if -1 in base:
+            if num < 0:
+                d[0] = 1
+                num = abs(num)
+            
+        for i in range(len(base)):
+            while num % base[i] == 0 and base[i] != -1:
+                num /= base[i]
+                d[i] += 1
+        if num == 1:
+            return d
+        else:
+            return None
 
-        # Round the degrees to integers
-        x = np.round(x).astype(int)
-
-        # Ensure non-negative values for x
-        x[x < 0] = 0
-
-        return x
+F = Factorizer(17873, method="brillhart-morrison", patience=50)
+print(F.factorize())
